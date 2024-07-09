71fc6a5c35004af98d700f2ab758f334e2034606
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-07-09 21:16:53 +0900

    add hamburger-menu

--- scripts/main.js
+++ scripts/main.js
@@ -13,5 +13,13 @@ const io = new IntersectionObserver(callback);
 slideIn.forEach(el => io.observe(el));
 fadeIn.forEach(el => io.observe(el));
 
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('.head-nav');
+const mainContent = document.querySelectorAll('.section-jump');
 
+const menuOpen = function(){
+    nav.classList.toggle('open');
+}
 
+hamburgerMenu.addEventListener('click', menuOpen);
+mainContent.forEach(el => el.addEventListener('click', menuOpen));
