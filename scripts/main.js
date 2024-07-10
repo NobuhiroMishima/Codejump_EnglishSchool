02414aebd29fbde3eb184236a0013b86d7f1a2e8
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-07-11 01:30:55 +0900

    add comment js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
+//スライドインの要素選択（BBBが選ばれる理由）
 const slideIn = document.querySelectorAll('.slide-item');
+//フェードインの要素選択（受講生の声）
 const fadeIn = document.querySelectorAll('.fade-in-item');
 
+//スライドインとフェードインの要素にinviewを付与
 const callback = function(entries){
     entries.forEach(entry => {
         if(entry.isIntersecting){
@@ -9,17 +12,24 @@ const callback = function(entries){
     });
 }
 
+//IntersectionObserverをインスタンス化
 const io = new IntersectionObserver(callback);
+
+//2つの要素を監視
 slideIn.forEach(el => io.observe(el));
 fadeIn.forEach(el => io.observe(el));
 
+
+//ハンバーガーメニューとハンバーガーメニューメニュー内のボタンの要素を選択
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const nav = document.querySelector('.head-nav');
 const mainContent = document.querySelectorAll('.section-jump');
 
+//header-navの要素にopenを付与
+const nav = document.querySelector('.head-nav');
 const menuOpen = function(){
     nav.classList.toggle('open');
 }
 
+//2つの要素をクリックしたときにtoggleのイベント発火
 hamburgerMenu.addEventListener('click', menuOpen);
 mainContent.forEach(el => el.addEventListener('click', menuOpen));
